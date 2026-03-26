94e08c7d37f650fcfee31f5ba6c03baa2e199237
tomMore07 <tomas.morera01@usa.edu.co>
2026-03-26 00:44:57 -0500

    Add files via upload

---
+++ Automotriz/Motor.java
@@ -0,0 +1,24 @@
+package automotriz;
+
+public class Motor {
+	
+	private String tipo;
+	
+	//constructor
+	public Motor(String tipo) {
+		super();
+		this.tipo = tipo;
+		// TODO Auto-generated constructor stub
+	}
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	@Override
+	public String toString() {
+		return " Tipo " + tipo;
+	}
+}
