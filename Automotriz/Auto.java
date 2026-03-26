eea1cef6ae264b1d2355d486017c898c61ddc25f
tomMore07 <tomas.morera01@usa.edu.co>
2026-03-26 00:33:51 -0500

    Update Auto.java

--- Automotriz/Auto.java
+++ Automotriz/Auto.java
@@ -1,34 +1,82 @@
 package automotriz;
 
-//no se como descargar esta libreria y no me sirvio jeje
-//import java.util.ArayList;
-
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class Auto {
+	
 	private String marca;
 	private int modelo;
 	private Motor motor;
 	private Cliente cliente;
-	private List<Llanta> listaLlantas;
+	private List<Llanta> llantas;
 	
 	
-	public Auto(String marca, int modelo, Cliente cliente) {
+	
+	
+	public Auto(String marca, int modelo, Cliente cliente, String tipoMotor) {
 		super();
 		this.marca = marca;
 		this.modelo = modelo;
-		this.cliente = cliente;
+		//Agregacion
+		this.cliente = cliente;	
+		//Composicion
+		this.motor = new Motor(tipoMotor);
+		//Composicion
+		this.llantas = new ArrayList<Llanta>();
 	}
 	
 	public void agregarLlanta(Llanta llanta) {
-		this.listaLlantas.add(llanta);
+		this.llantas.add(llanta);
+	}
+	
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(int modelo) {
+		this.modelo = modelo;
+	}
+
+	public Motor getMotor() {
+		return motor;
+	}
+
+	public void setMotor(Motor motor) {
+		this.motor = motor;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public List<Llanta> getLlantas() {
+		return llantas;
+	}
+
+	public void setLlantas(List<Llanta> llantas) {
+		this.llantas = llantas;
 	}
 
 	@Override
 	public String toString() {
-		return "Propietario del Vehiculo " + this.cliente + " Marca del auto: " + this.marca + "  Modelo: " + this.modelo 
-				+ "  Motor: " + this.motor + " Referencia de Llantas: " + this.listaLlantas;
+		return "Propietario del Vehiculo: " + cliente + "| Marca del auto: " + marca + "| Modelo: " + modelo 
+				+ "| Motor: " + motor + "| Referencia de Llantas: " + llantas;
 	}
+	
+	
 	
 }
