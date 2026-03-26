5635fa0f60e3cbf843ac117928feb15d413fd782
Sara D <sara.diaz05@usa.edu.co>
2026-03-25 21:51:59 -0500

    Update StistemaCitas.java

--- clinica/StistemaCitas.java
+++ clinica/StistemaCitas.java
@@ -4,7 +4,36 @@ package clinica;
 	import java.util.ArrayList;
 
 public class SistenmaCitas {
-	private List<>
+	
+		private List<Paciente> listaPaciente;
+		private List<Medico> listaMedico;
+		private List<Cita> listaCita;	
+		
+		public SistemaCitas(List<Paciente> listaPaciente, List<Medico> listaMedico, List<Cita> listaCita) {
+			super();
+			this.listaPaciente = new ArrayList<>();
+			this.listaMedico = new ArrayList<>();
+			this.listaCita = new ArrayList<>();
+		}
+	
 
+		public void agregarCita(Cita cita) {
+			this.listaCita.add(cita);
+			
+		}
+		public void agregarMedico(Medico medico) {
+			this.listaMedico.add(medico);
+			
+		}
+		public void agregarPaciente(Paciente paciente) {
+			this.listaPaciente.add(paciente);
+			
+		}
+		
+		
+		public List<Cita> buscarPorPaciente(Paciente nombre){
+			
+		}
+		
 }
 
