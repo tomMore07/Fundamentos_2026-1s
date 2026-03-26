0e1bcef0a398d599f8003846b4cd0c91c4489fb3
Sara D <sara.diaz05@usa.edu.co>
2026-03-25 21:06:26 -0500

    Update Paciente.java

--- clinica/Paciente.java
+++ clinica/Paciente.java
@@ -11,12 +11,37 @@ public class Paciente {
 	private int telefono;
 	
 	
-	public Paciente(int identificacion, String nombre, Date fechaNacimiento, String direccion, int telefono) {
+	public Paciente(int identificacion, String nombre) {
 		super();
 		this.identificacion = identificacion;
 		this.nombre = nombre;
-		this.fechaNacimiento = fechaNacimiento;
-		this.direccion = direccion;
-		this.telefono = telefono;
 	}
+
+
+	public int getIdentificacion() {
+		return identificacion;
+	}
+
+
+	public void setIdentificacion(int identificacion) {
+		this.identificacion = identificacion;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Nombre del paciente:" + this.nombre + " Identificación: "+ this.identificacion;
+	}
+
 }
