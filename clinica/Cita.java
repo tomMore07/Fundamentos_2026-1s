7577a3ac7564eb7058913c59a4e620c03c2b3a77
tomMore07 <tomas.morera01@usa.edu.co>
2026-03-25 21:20:35 -0500

    Update Cita.java

--- clinica/Cita.java
+++ clinica/Cita.java
@@ -7,6 +7,7 @@ public class Cita {
 	private LocalDateTime fechaYhora;
 	private Paciente paciente;
 	private Medico medico;
+	private Boolean estado;
 	
 	
 	public Cita(LocalDateTime fechaYhora, Paciente paciente, Medico medico) {
@@ -14,5 +15,11 @@ public class Cita {
 		this.fechaYhora = fechaYhora;
 		this.paciente = paciente;
 		this.medico = medico;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Fecha y Hora: "+ fechaYhora + " Paciente: "+ paciente + " Medico: " 
+				+ medico;
 	}
 }
