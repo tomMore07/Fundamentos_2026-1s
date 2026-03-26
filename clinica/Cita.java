f64aae11d473cba1b57f13fe388474a6aefc55b9
Sara D <sara.diaz05@usa.edu.co>
2026-03-25 22:28:31 -0500

    Update Cita.java

--- clinica/Cita.java
+++ clinica/Cita.java
@@ -16,6 +16,40 @@ public class Cita {
 		this.paciente = paciente;
 		this.medico = medico;
 	}
+
+	public LocalDateTime getFechaYhora() {
+		return fechaYhora;
+	}
+
+
+	public void setFechaYhora(LocalDateTime fechaYhora) {
+		this.fechaYhora = fechaYhora;
+	}
+
+
+	public Medico getMedico() {
+		return medico;
+	}
+
+
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
+
+
+	public Boolean getEstado() {
+		return estado;
+	}
+
+
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
