15018abb1e8475036691ad8013f63df4e5dfa2d0
Sara D <sara.diaz05@usa.edu.co>
2026-03-25 20:01:58 -0500

    Update Paciente.java

--- clinica/Paciente.java
+++ clinica/Paciente.java
@@ -1,1 +1,22 @@
+package clinica;
 
+import java.util.Date;
+
+public class Paciente {
+	
+	private int identificacion;
+	private String nombre;
+	private Date fechaNacimiento;
+	private String direccion;
+	private int telefono;
+	
+	
+	public Paciente(int identificacion, String nombre, Date fechaNacimiento, String direccion, int telefono) {
+		super();
+		this.identificacion = identificacion;
+		this.nombre = nombre;
+		this.fechaNacimiento = fechaNacimiento;
+		this.direccion = direccion;
+		this.telefono = telefono;
+	}
+}
