f447e55637b53cf8924f20706596a1e16a539bd0
Sara D <sara.diaz05@usa.edu.co>
2026-04-03 12:37:32 -0500

    Create Examen.java

---
+++ Hospital/Examen.java
@@ -0,0 +1,46 @@
+package lab5;
+
+import java.time.LocalDate;
+
+
+public class Examen {
+	
+		private String id;
+
+		private LocalDate fecha;
+
+		private String tipo;
+
+		
+
+		
+
+		public Examen(String id, LocalDate fecha, String tipo) {
+
+			super();
+
+			this.id = id;
+
+			this.fecha = fecha;
+
+			this.tipo = tipo;
+
+		}
+
+		
+
+		@Override
+
+		public String toString() {
+
+			
+
+			return "Identificador del exámen: "+ this.id + "Fecha del exámen" 
+
+			+ this.fecha+ " Tipo del exámen" + this.tipo  ;
+
+		}
+
+		
+
+	}
