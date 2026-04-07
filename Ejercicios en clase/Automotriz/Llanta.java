e9a054aa9931025282bb710bbb0650d9b5c48af9
Sara D <sara.diaz05@usa.edu.co>
2026-04-07 07:34:57 -0500

    Update Llanta.java

--- Ejercicios en clase/Automotriz/Llanta.java
+++ Ejercicios en clase/Automotriz/Llanta.java
@@ -1,1 +1,24 @@
+package automotriz;
 
+public class Llanta {
+
+	private String marca;
+	
+	//constructor
+	public Llanta(String marca) {
+		super();
+		this.marca = marca;
+		// TODO Auto-generated constructor stub
+	}
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	@Override
+	public String toString() {
+		return " Marca " + this.marca;
+	}
+}
