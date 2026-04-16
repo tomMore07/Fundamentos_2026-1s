adeca8b35dd86b4bde09abb7df8f998cb0c5ebc2
tomMore07 <tomas.morera01@usa.edu.co>
2026-04-15 22:04:04 -0500

    Add files via upload

---
+++ LABORATORIOS/lab 6/punto 2/Moto.java
@@ -0,0 +1,29 @@
+package ejercicio2;
+
+public class Moto extends Vehiculo {
+
+	private String tipo;
+	
+	public Moto(String placa, String marca, int velMax) {
+		super(placa, marca, velMax);
+		this.tipo = tipo;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void mover(){
+		
+	}
+	
+	
+	public int calcularPeaje() {
+		int peaje = 0;
+		return peaje;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Moto: "+ super.toString()+ " Tipo: "+ this.tipo;
+	}
+
+}
