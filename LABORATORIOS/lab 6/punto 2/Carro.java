f43acafc40c09877ee12b044d6208e1e9b823a43
Sara D <sara.diaz05@usa.edu.co>
2026-04-15 21:40:53 -0500

    Update Carro.java

--- LABORATORIOS/lab 6/punto 2/Carro.java
+++ LABORATORIOS/lab 6/punto 2/Carro.java
@@ -1,1 +1,27 @@
+package ejercicio2;
+
+public class Carro extends Vehiculo {
+	private int numeroPuertas;
+
+	public Carro(String placa, String marca, int velMax, int numeroPuertas) {
+		super(placa, marca, velMax);
+		this.numeroPuertas = numeroPuertas;
+	}
+
+	public void mover() {
+		
+	}
+	
+	public int calcularPeaje() {
+		int peaje = 23800;
+		return peaje;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Carro: "+ super.toString()+ " Numero de puertas: "+ this.numeroPuertas;
+	}
+	
+}
 
