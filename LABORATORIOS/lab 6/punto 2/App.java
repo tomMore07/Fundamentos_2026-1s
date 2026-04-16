ac302cad30abec94b120af6ce036155745ec29d9
Sara D <sara.diaz05@usa.edu.co>
2026-04-15 22:55:21 -0500

    Update App.java

--- LABORATORIOS/lab 6/punto 2/App.java
+++ LABORATORIOS/lab 6/punto 2/App.java
@@ -7,14 +7,17 @@ public class App {
 		Carro carro =new Carro("YN09V","Porsche", 280, 2);
 		System.out.println(carro);
 		System.out.println("peaje:$"+ carro.calcularPeaje());
+		carro.mover();
 		
 		Moto moto = new Moto("89BKU9","Royal Enfield", 190,"deportiva");
 		System.out.println(moto);
 		System.out.println("peaje:$"+moto.calcularPeaje());
+		moto.mover();
 		
 		Camion camion =new Camion("AGY878","marca", 100, 89, 9, TipoCamion.F350);
 		System.out.println(camion);
 		System.out.println("peaje:$"+ camion.calcularPeaje());
+		camion.mover();
 	}
 
 }
