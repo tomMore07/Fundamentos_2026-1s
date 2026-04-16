437c56fa0f3202c3e5ff4a941fa6ae1ae4e25042
Sara D <sara.diaz05@usa.edu.co>
2026-04-15 22:42:25 -0500

    Update App.java

--- LABORATORIOS/lab 6/punto 2/App.java
+++ LABORATORIOS/lab 6/punto 2/App.java
@@ -1,1 +1,21 @@
+package ejercicio2;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Carro carro =new Carro("placa","marca", 280, 2);
+		System.out.println(carro);
+		System.out.println("peaje: $"+ carro.calcularPeaje());
+		
+		Moto moto = new Moto("placa","marca", 190,"tipo");
+		System.out.println(moto);
+		System.out.println("peaje:$"+moto.calcularPeaje());
+		
+		Camion camion =new Camion("AGY878","marca", 100, 89, 9, TipoCamion.F350);
+		System.out.println(camion);
+		System.out.println("peaje: $"+ camion.calcularPeaje());
+	}
+
+}
 
