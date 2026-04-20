be498d235b18cf76ea2f637b28647305099718a5
tomMore07 <tomas.morera01@usa.edu.co>
2026-04-20 16:17:24 -0500

    Update AppFigura.java

--- Ejercicios en clase/Figuras/AppFigura.java
+++ Ejercicios en clase/Figuras/AppFigura.java
@@ -11,6 +11,15 @@ public class AppFigura {
 		
 		Cuadrado cuadrado1 = new Cuadrado("Magenta", 10, 10);
 		
+		Triangulo triangulo = new Triangulo("Rojo", 20, 60);
+		
+		Circulo circulo1 = new Circulo("Verde", 100);
+		
+		listaFiguras.add(circulo1);
+		listaFiguras.add(triangulo);
+		listaFiguras.add(circulo1);
+		
+		System.out.println(listaFiguras.get(1).calcularArea());
 		
 
 	}
