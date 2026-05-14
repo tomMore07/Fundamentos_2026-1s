c53616e6c1a76cb34c3166eecc32bae008406f46
tomMore07 <tomas.morera01@usa.edu.co>
2026-05-14 08:53:33 -0500

    Update Cliente.java

--- Corte3/tickets/modelodto/Cliente.java
+++ Corte3/tickets/modelodto/Cliente.java
@@ -1,1 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tickets.modelo.dto;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author tomy
+ */
+public class Cliente implements Serializable {
+    private int documento;
+    private String nombre;
+    private int cantBoletos;
+
+    public int getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(int documento) {
+        this.documento = documento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantBoletos() {
+        return cantBoletos;
+    }
+
+    public void setCantBoletos(int cantBoletos) {
+        this.cantBoletos = cantBoletos;
+    }
+    
+    
+}
 
