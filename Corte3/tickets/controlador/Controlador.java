9fe8222280f2b2fac55f586f1757c4c23f70ff1a
Sara D <sara.diaz05@usa.edu.co>
2026-05-19 19:58:53 -0500

    Update Controlador.java

--- Corte3/tickets/controlador/Controlador.java
+++ Corte3/tickets/controlador/Controlador.java
@@ -1,1 +1,93 @@
 
+package com.mycompany.tickets.controlador;
+
+import com.mycompany.tickets.modelo.dto.Cliente;
+import com.mycompany.tickets.modeloDAO.ClienteDAO;
+import com.mycompany.tickets.vista.ClienteVista;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+
+
+public class ClienteControlador implements ActionListener{
+    private ClienteVista vista;
+    private ClienteDAO modelo;
+    private Cliente cliente;
+    
+    public ClienteControlador(ClienteVista vista){
+        this.cliente = cliente;
+        this.modelo = new ClienteDAO();
+        
+        
+        this.vista.buscar.addActionListener(this);
+        this.vista.eliminar.addActionListener(this);
+        this.vista.modificar.addActionListener(this);
+        this.vista.registrar.addActionListener(this);
+        
+        this.vista.setVisible(true);
+    }
+      
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource().equals(this.vista.registrar))
+            registrar();
+        if(e.getSource().equals(this.vista.modificar))
+            modificar();
+        if(e.getSource().equals(this.vista.buscar))
+            buscar();
+        if(e.getSource().equals(this.vista.eliminar))
+            eliminar();
+    }
+
+    private void registrar() {
+        cliente = new Cliente();
+        
+        int identificacion = Integer.valueOf(this.vista.documentoIn.getText());
+        String nombre = this.vista.nombre.getText();
+        int boletosBox = (int) this.vista.boletosBox.getSelectedItem();
+        
+        char localidad= '\0';
+        if(this.vista.localidadA.isSelected()){
+            localidad = 'A';
+        }else if(this.vista.localidadB.isSelected()){
+            localidad = 'B';
+        }else if(this.vista.localidadC.isSelected()){
+            localidad = 'C';
+        }else if(this.vista.localidadD.isSelected()){
+            localidad = 'D';
+        }else{ localidad = '\0';}
+           
+        
+                
+        cliente.setIdentificacion(identificacion);
+        cliente.setNombre(nombre);
+        cliente.setCantBoletos(boletosBox);
+        cliente.setLocalidad(localidad);
+        
+        if(modelo.registrar(cliente))
+            JOptionPane.showMessageDialog(null, "Se registro exitosamente su boleto");
+            else
+            JOptionPane.showMessageDialog(null, "F vv te quedaste sin boletos");
+            
+        
+    }
+
+    private void modificar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    private void buscar() {
+        int documento = Integer.valueOf(this.vista.documentoIn.getText());
+        
+        cliente = modelo.buscar(documento);
+        if(cliente == null)
+        JOptionPane.showMessageDialog(null, "Mor no estas registrade");
+    else this.vista.nombre.setText(cliente.getNombre());
+         this.vista.boletosBox.setText(cliente.get());
+
+    }
+    private void eliminar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
+
