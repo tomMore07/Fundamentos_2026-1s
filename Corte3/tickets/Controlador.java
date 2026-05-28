b22ff3160c46b60feaafe50b025a2bdeccc4dc32
Sara D <sara.diaz05@usa.edu.co>
2026-05-27 20:26:24 -0500

    Update Controlador.java

--- Corte3/tickets/Controlador.java
+++ Corte3/tickets/Controlador.java
@@ -80,8 +80,10 @@ public class ClienteControlador implements ActionListener {
 
         if (modelo.registrar(cliente))
             JOptionPane.showMessageDialog(null, "¡Boleto registrado exitosamente!");
-        else
+        else if (modelo.buscar(cliente.getDocumento()) != null)
             JOptionPane.showMessageDialog(null, "Ya existe un registro con ese documento, o te quedaste sin boleto :(");
+         else
+        JOptionPane.showMessageDialog(null, "No hay suficientes asientos disponibles en esa localidad.");
     }
 
 
