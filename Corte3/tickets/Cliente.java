dd08db9e65edf83f12a0ed4c913a1617682dc3e2
tomMore07 <tomas.morera01@usa.edu.co>
2026-05-27 20:09:56 -0500

    Update Cliente.java

--- Corte3/tickets/Cliente.java
+++ Corte3/tickets/Cliente.java
@@ -11,41 +11,39 @@ import java.io.Serializable;
  * @author tomy
  */
 public class Cliente implements Serializable {
+
     private int documento;
     private String nombre;
     private int cantBoletos;
+    private char localidad;   
 
-    public int getDocumento() {
-        return documento;
+    public int getDocumento(){
+        return documento; 
+    }
+    public void setDocumento(int documento){
+        this.documento = documento; 
     }
 
-    public void setDocumento(int documento) {
-        this.documento = documento;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getCantBoletos() {
-        return cantBoletos;
-    }
-
-    public void setCantBoletos(int cantBoletos) {
-        this.cantBoletos = cantBoletos;
-    }
-    public char getLocalidad() {
-        return localidad;
-    }
-
-    public void setLocalidad(char localidad) {
-        this.localidad = localidad;
+    public String getNombre(){
+        return nombre; 
     }
     
+    public void setNombre(String nombre){
+        this.nombre = nombre; 
+    }
+
+    public int getCantBoletos(){
+        return cantBoletos; 
+    }
     
+    public void setCantBoletos(int cantBoletos){
+        this.cantBoletos = cantBoletos; 
+    }
+
+    public char getLocalidad(){
+        return localidad; 
+    }
+    
+    public void setLocalidad(char localidad){
+        this.localidad = localidad; }
 }
-
